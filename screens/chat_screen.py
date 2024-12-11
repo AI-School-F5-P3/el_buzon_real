f7abf9f37523744c6a86f927fdcd3e8c19fc013e
Lisy <damaliamzn@gmail.com>
2024-12-11 13:26:19 +0100

    add box message chat

--- screens/chat_screen.py
+++ screens/chat_screen.py
@@ -11,56 +11,19 @@ load_dotenv()
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
 def show():
-    st.title("Pide tus regalos a Santa!🎅")
+    st.title("Habla con ayudante de los Reyes Mágos🎅")
     
     # Campo de entrada para el prompt del usuario
-    user_prompt = st.text_input("Escribe tu mensaje para el ayudante de Santa:")
+    user_prompt = st.text_input("Escribe tu mensaje:")
     
     # Botón para enviar el prompt
     if st.button("Enviar"):
         if user_prompt:
             # Crear instrucciones del chatbot
-            instructions = create_chatbot_instructions(user_prompt)
+            instructions = create_chatbot_instructions()
             
             # Generar respuesta del chat
-            response = generate_chat_response(instructions)
-            
-            # Mostrar la respuesta
-            st.write("Respuesta del ayudante de Santa:")
-            st.write(response)
-        else:
-            st.write("Por favor, escribe un mensaje.")
-    
-    # Botón para cerrar y volver a la página principal
-    if st.button("Cerrar y volver a la página principal"):
-        st.session_state.current_screen = "welcome"
-=======
-import streamlit as st
-from openai import AzureOpenAI
-import os
-from chat.chatbot import create_chatbot_instructions, generate_chat_response, save_gift_list_to_mongodb
-from dotenv import load_dotenv
-
-load_dotenv()
-
-
-# Configura tu clave de API de OpenAI
-openai.api_key = os.getenv('OPENAI_API_KEY')
-
-def show():
-    st.title("Pide tus regalos a Santa!🎅")
-    
-    # Campo de entrada para el prompt del usuario
-    user_prompt = st.text_input("Escribe tu mensaje para el ayudante de Santa:")
-    
-    # Botón para enviar el prompt
-    if st.button("Enviar"):
-        if user_prompt:
-            # Crear instrucciones del chatbot
-            instructions = create_chatbot_instructions(user_prompt)
-            
-            # Generar respuesta del chat
-            response = generate_chat_response(instructions)
+            response = generate_chat_response(instructions, user_prompt)
             
             # Mostrar la respuesta
             st.write("Respuesta del ayudante de Santa:")
