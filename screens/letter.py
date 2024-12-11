8d069dbb1dbe7f46762512cacd73f05767fd8028
jvazquez28 <jvazquez28@gmail.com>
2024-12-11 12:51:30 +0100

    organizacion dentro de dev

--- screens/letter.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-def show():
-    st.title("Your Special Letter to Santa 📝")
-    # Placeholder for letter generation
-    if st.button("Save My Letter"):
-        st.session_state.current_screen = "confirmation"
-        st.rerun()
